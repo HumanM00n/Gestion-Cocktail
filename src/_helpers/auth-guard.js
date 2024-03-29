0e224ac1257763154abeb32d27efa947b2e0a3c1
HumanM00n <mrouy92@gmail.com>
2024-03-29 18:59:36 +0100

    UPDATE

--- src/_helpers/auth-guard.js
+++ src/_helpers/auth-guard.js
@@ -1,13 +1,1 @@
-import router from "@/router"
-import { routeLocationKey } from "vue-router"
 
-export function AuthGuard(to) {
-    let token = localStorage.getItem('token')
-    console.log(token)
-
-    if(token) {
-        return true
-    }
-
-    router.push('/')
-}
